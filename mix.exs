cc928abdc6d761d76113067432e9d6d0fcb2507b
Ray <7869818+rayrrr@users.noreply.github.com>
2019-11-12 21:54:25 -0500

    bump pow version to support PowInvitation

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule PhotoGallery.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:pow, "~> 1.0.0"},
+      {:pow, "~> 1.0.4"},
       {:bodyguard, "~> 2.2"},
       {:arc, "~> 0.11.0"},
       {:arc_ecto, "~> 0.11.1"},
